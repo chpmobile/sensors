70e6da8f77691b13aff1c5e5ca92a34d464996aa
chpmobile <papc2_12@uni.worc.ac.uk>
2015-12-03 16:59:42 +0000

    c
    c

--- www/index.js
+++ www/index.js
@@ -1,6 +1,6 @@
 var watchID;
-var accelerometerOptions = { frequency: 2000 };  // Update every 2 seconds
-accelerometerOptions.frequency = 3000; //changed my mind - now 3 seconds
+var accelerometerOptions;  // Update every 2 seconds
+//accelerometerOptions.frequency = 3000; //changed my mind - now 3 seconds
 
 
 //when the page is created...
@@ -58,8 +58,8 @@ function accelerometerError() {
 function updateFreq(freq) {
 	//do something to update freq. here.
 
-	if ($('value').val(freq) > 5000) accelerometerOptions = 5000;
-	else accelerometerOptions = 1000;
+	if ($('value').val(freq) > 5000) accelerometerOptions.frequency = 5000;
+	else accelerometerOptions.frequency = 1000;
 	
 }
 
